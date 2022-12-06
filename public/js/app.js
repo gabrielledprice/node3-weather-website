1829f3ba50199b60a01905f5df6576b6b79160a2
gabdprice <110576773+gabrielledprice@users.noreply.github.com>
2022-12-06 15:29:13 -0800

    updated css, added functionality showing weather

--- public/js/app.js
+++ public/js/app.js
@@ -6,26 +6,30 @@
 //     })
 // })
 
-
-
-
 var weatherForm = document.getElementById("search");
 var searchElement = document.querySelector("input");
+var messageOne = document.getElementById("message-1");
+var messageTwo = document.getElementById("message-2");
 
+messageOne.textContent = "Loading...";
+messageTwo.textContent = "";
 if (weatherForm) {
   weatherForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const location = searchElement.value;
     fetch("http://localhost:3000/weather?address=" + location).then(
-        (response) => {
-          response.json().then((data) => {
-            if (data.error) {
-              return console.log(data.error);
-            }
-            console.log(data.address, data.forecast);
-          });
-        }
-      );
-    };
-  );
+      (response) => {
+        response.json().then((data) => {
+          if (data.error) {
+            return (messageOne.textContent = data.error);
+          }
+          const { temperature, Description, rain } = data.forecast;
+          console.log(data);
+          messageOne.textContent = data.address;
+          // messageTwo.textContent = JSON.stringify(data.forecast);
+          messageTwo.textContent = `The temperature is ${temperature}, and it is ${Description[0]} outside. There is a ${rain} percent chance of rain.`;
+        });
+      }
+    );
+  });
 }
